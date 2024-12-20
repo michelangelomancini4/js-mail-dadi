1a8bc09be3f598657bdb1bac71c0b084659a135d
michelangelomancini <michelangelomancini8@gmail.com>
2024-12-20 14:53:42 +0100

    fase iniziale creazione logica

---
+++ js-mail/jvs.js
@@ -0,0 +1,14 @@
+// Mail
+// Crea una lista di email di invitati ad una festa. 
+
+const listaMail = ["giovanni@tiscali.it","andrea@tiscali.it", "luca@tiscali.it", "sebastiano@tiscali.it", "paolo@tiscali.it", "dario@tiscali.it",  ]
+console.log(listaMail);
+
+// Chiedi all’utente la sua email,
+
+let mailUtente = prompt ("Inserisci la tua mail per vedere se sei nella lista")
+
+//  controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+// Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
+// Si può fare? Certo che si basta ragionare un po’.
+// Nota: Non è necessario provvedere alla validazione delle email
